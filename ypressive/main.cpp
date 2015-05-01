3553beea035fa4b2cbb48fb386a91f4045a3dbc4
Юра <Yura@Mercury.local>
2015-05-01 21:02:30 +0300

    ideas

---
+++ ypressive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ypressive
+//
+//  Created by Юра on 01.05.15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
